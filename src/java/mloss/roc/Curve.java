63c84419c57d8a5b2250454e400038ef391e4cfb
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2012-03-20 19:18:52 -0500

    First "working" version of Curve.java and tests.
    * Documented concepts
    * Updated to correctly use counts of both positives and negatives
    * Implemented and tested
      * rocPoint
      * rocArea (not general enough, but need better tests)

--- src/java/mloss/roc/Curve.java
+++ src/java/mloss/roc/Curve.java
@@ -6,13 +6,49 @@ package mloss.roc;
 
 
 /**
- * Conceptually a curve is an ordered set of confusion matrices, one for
- * each element of a ranking plus a zero one to start.
- * TODO
+ * Conceptually a curve is a list of confusion matrices, one for each
+ * element of a ranking plus a zero one to start.  This representation
+ * contains all the information needed to compute everything about ROC
+ * and PR curves.  If you have n points in your ranking, you need n + 1
+ * confusion matrices, one in response to each element of the ranking
+ * and a "null" (zero) one to start.  A confusion matrix is a 2-by-2
+ * table describing the classification result that would occur if one
+ * chose the classification threshold "below" the current element of the
+ * ranking.  A confusion matrix contains four numbers: the number of
+ * true positives, the number of false positives, the number of false
+ * negatives, and the number of true negatives.  See the following
+ * table.
+ *
+ * <pre>
+ *            |              Actual               |
+ * Classified | Positive        | Negative        |
+ * ------------------------------------------------
+ * Positive   | True Positive   | False Positive  |
+ * Negative   | False Negative  | True Negative   |
+ * ------------------------------------------------
+ *            | Total Positives | Total Negatives |
+ * </pre>
+ *
+ * Although each confusion matrix involves four numbers, each can
+ * actually be represented by two numbers plus two totals (shared among
+ * all matrices): the number of true positives so far in the ranking,
+ * the number of false positives so far in the ranking,
+ * the total number of positives in the ranking, and the total
+ * number of negatives in the ranking.  This is possible
+ * because the following identities allow one to recover each confusion
+ * matrix.
+ *
+ * <code>
+ * true positives = (true positive counts list)[ranking index]
+ * false positives = (false positive counts list)[ranking index]
+ * false negatives = (total positives) - (true positives)
+ * true negatives = (total negatives) - (false positives)
+ * </code>
  */
 class Curve {
 
     protected int[] truePositiveCounts;
+    protected int[] falsePositiveCounts;
     protected int totalPositives;
     protected int totalNegatives;
 
@@ -27,13 +63,14 @@ class Curve {
      * TODO
      */
     void buildCounts(int[] rankedLabels) {
-        // Allocate space for n + 1 points.  There is one point in
-        // between each ranked label (n - 1) and two points for start
-        // and end.
+        // Allocate space for n + 1 points.  There is one point after
+        // each element in the ranking and a zero one to start.
         truePositiveCounts = new int[rankedLabels.length + 1];
+        falsePositiveCounts = new int[rankedLabels.length + 1];
 
         // Initial values
         truePositiveCounts[0] = 0;
+        falsePositiveCounts[0] = 0;
         totalPositives = 0;
         totalNegatives = 0;
 
@@ -45,30 +82,41 @@ class Curve {
                 totalNegatives++;
             }
             truePositiveCounts[labelIndex + 1] = totalPositives;
+            falsePositiveCounts[labelIndex + 1] = totalNegatives;
         }
     }
 
     /**
      * TODO
      */
-    int[] confusionMatrix(int rankNumber) {
+    public int[] confusionMatrix(int rankNumber) {
         int truePositives = truePositiveCounts[rankNumber];
-        int falsePositives = rankNumber - truePositives;
+        int falsePositives = falsePositiveCounts[rankNumber];
         int falseNegatives = totalPositives - truePositives;
         int trueNegatives = totalNegatives - falsePositives;
         return new int[] {truePositives, falsePositives, falseNegatives, trueNegatives};
     }
 
-    double[] point(int rankNumber) {
-        // TODO
-        return new double[2];
+    public double[] rocPoint(int rankNumber) {
+	double falsePositiveRatio = (double) falsePositiveCounts[rankNumber] / (double) totalNegatives;
+	double truePositiveRatio = (double) truePositiveCounts[rankNumber] / (double) totalPositives;
+        return new double[] {falsePositiveRatio, truePositiveRatio};
     }
 
     /**
      * TODO
      */
-    double area() {
-        // TODO
-        return 0.0;
+    public double rocArea() {
+	// TODO make work for trapezoids and convex hull
+	// There is only a new rectangle when the x-value (FPR) changes
+	double area = 0.0;
+	for (int countIndex = 1; countIndex < truePositiveCounts.length; countIndex++) {
+	    if (falsePositiveCounts[countIndex] > falsePositiveCounts[countIndex - 1]) {
+		double base = (double) (falsePositiveCounts[countIndex] - falsePositiveCounts[countIndex - 1]) / (double) totalNegatives;
+		double height = (double) truePositiveCounts[countIndex] / (double) totalPositives;
+		area += base * height;
+	    }
+	}
+        return area;
     }
 }
--- test/java/mloss/roc/CurveTest.java
+++ test/java/mloss/roc/CurveTest.java
@@ -18,13 +18,16 @@ public class CurveTest {
     // Test data
 
     static final int[] labelsWorst = {0, 0, 1, 1, 1, 1, 1};
-    static final int[] labelsWorst_counts = {0, 0, 0, 1, 2, 3, 4, 5};
+    static final int[] labelsWorst_posCounts = {0, 0, 0, 1, 2, 3, 4, 5};
+    static final int[] labelsWorst_negCounts = {0, 1, 2, 2, 2, 2, 2, 2};
 
     static final int[] labelsAverage = {1, 0, 1, 1, 0, 0, 1, 0, 0, 1};
-    static final int[] labelsAverage_counts = {0, 1, 1, 2, 3, 3, 3, 4, 4, 4, 5};
+    static final int[] labelsAverage_posCounts = {0, 1, 1, 2, 3, 3, 3, 4, 4, 4, 5};
+    static final int[] labelsAverage_negCounts = {0, 0, 1, 1, 1, 2, 3, 3, 4, 5, 5};
 
     static final int[] labelsBest = {1, 1, 0, 0, 0};
-    static final int[] labelsBest_counts = {0, 1, 2, 2, 2, 2};
+    static final int[] labelsBest_posCounts = {0, 1, 2, 2, 2, 2};
+    static final int[] labelsBest_negCounts = {0, 0, 0, 1, 2, 3};
 
     Curve curve;
 
@@ -35,17 +38,20 @@ public class CurveTest {
     /** Tests {@link Curve.buildCounts(int[])}. */
     @Test public void testBuildCountsFromHardLabels() {
         Curve curve = new Curve(labelsWorst);
-        assertArrayEquals(labelsWorst_counts, curve.truePositiveCounts);
+        assertArrayEquals(labelsWorst_posCounts, curve.truePositiveCounts);
+        assertArrayEquals(labelsWorst_negCounts, curve.falsePositiveCounts);
         assertEquals(5, curve.totalPositives);
         assertEquals(2, curve.totalNegatives);
 
         curve = new Curve(labelsAverage);
-        assertArrayEquals(labelsAverage_counts, curve.truePositiveCounts);
+        assertArrayEquals(labelsAverage_posCounts, curve.truePositiveCounts);
+        assertArrayEquals(labelsAverage_negCounts, curve.falsePositiveCounts);
         assertEquals(5, curve.totalPositives);
         assertEquals(5, curve.totalNegatives);
 
         curve = new Curve(labelsBest);
-        assertArrayEquals(labelsBest_counts, curve.truePositiveCounts);
+        assertArrayEquals(labelsBest_posCounts, curve.truePositiveCounts);
+        assertArrayEquals(labelsBest_negCounts, curve.falsePositiveCounts);
         assertEquals(2, curve.totalPositives);
         assertEquals(3, curve.totalNegatives);
     }
@@ -70,8 +76,8 @@ public class CurveTest {
         }
     }
 
-    /** Tests {@link Curve.point(int)}. */
-    @Test public void testPoint() {
+    /** Tests {@link Curve.rocPoint(int)}. */
+    @Test public void testRocPoint() {
         // Remember FPR horizontal, TPR vertical
         // Do these in fractions for human readability and floating-point precision
         double[][] expected = {
@@ -88,12 +94,12 @@ public class CurveTest {
             {1.0, 1.0}  // 10
         };
         for (int expectedIndex = 0; expectedIndex < expected.length; expectedIndex++) {
-            assertArrayEquals(expected[expectedIndex], curve.point(expectedIndex), TOLERANCE);
+            assertArrayEquals(expected[expectedIndex], curve.rocPoint(expectedIndex), TOLERANCE);
         }
     }
 
-    /** Tests {@link Curve.area()}. */
-    @Test public void testArea() {
+    /** Tests {@link Curve.rocArea()}. */
+    @Test public void testRocArea() {
         // Points: Rectangles (w * h = area):
         // 1-2: 0.2 * 0.2 = 1.0 / 25.0
         // 4-5: 0.2 * 0.6 = 3.0 / 25.0
@@ -103,6 +109,6 @@ public class CurveTest {
         // ---------------------------
         // Sum: 15.0 / 25.0 = 0.6
         double expected = 15.0 / 25.0;
-        assertEquals(expected, curve.area(), TOLERANCE);
+        assertEquals(expected, curve.rocArea(), TOLERANCE);
     }
 }
