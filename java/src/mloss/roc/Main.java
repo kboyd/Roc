31d3f9250ac439ea05f60ef2b19684142c5ca349
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2014-01-22 23:15:03 -0600

    Added options for file names.
    * Options for file names allow specification of the columns of interest
      in files (keys, scores, labels).
    * Options are parsed and checked but are currently ignored during
      processing.  They will not work until they are incorporated into
      improved file processing.
    * Added function to parse and handle file options.  Parsing is via
      regular expressions.
    * Added FileArgument which wraps a file name and its options.
    * Wrote help for file options with examples.
    * Updated copyright year in about string.

--- java/src/mloss/roc/Main.java
+++ java/src/mloss/roc/Main.java
@@ -18,17 +18,23 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import mloss.roc.util.NaiveCsvReader;
 
 
 // General idea is to read columns (scores or labels) from files and
-// then send them for appropriate processing (sorting, curve
+// then send them for appropriate processing (joining, sorting, curve
 // generation).
 
-// TODO specification of columns in CSV files. bracket notation after
-// filename on command line?
+// TODO handle file options
+// TODO project input CSVs
+// TODO join input CSVs
+// TODO connect input to Curve building
+// TODO check for empty input
+// TODO more (basic) examples in help
+
 
 /**
  * <p>Main reads the results of a binary classification and generates a
@@ -42,17 +48,16 @@ import mloss.roc.util.NaiveCsvReader;
  * be done to the output of a classification algorithm and so that
  * generating ROC/PR analyses are easy.  Indeed, this main will even
  * join scores and labels together database-style.</p>
- *
- * <p></p>
  */
 public class Main {
 
     public static final String aboutMessage =
         "Roc 0.1.0.  ROC and PR curves.\n" +
-        "Copyright (c) 2013 Roc Project.  This is free software; see the license.\n" +
+        "Copyright (c) 2014 Roc Project.  This is free software; see the license.\n" +
         "https://github.com/kboyd/Roc";
 
     // TODO replace license message with dumping license file from JAR
+    // (or similar "resource" access)
     public static final String licenseMessage =
         "Roc is free, open source software licensed under the BSD 2-clause (FreeBSD) license.";
 
@@ -80,7 +85,7 @@ public class Main {
     // Manually wrap the help text to 80 characters (column 89 for
     // non-indented, column 81 for indented)
     public static final String help =
-        "Roc.  Everything ROC and PR curves.\n\n" +
+        "                      Roc.  Everything ROC and PR curves.\n\n" +
 
         "SYNOPSIS\n\n" +
 
@@ -91,7 +96,15 @@ public class Main {
         "Reads the scores and labels of a binary classification result and prints ROC and\n" +
         "PR analysis reports.  When no options are given, this program operates as if\n" +
         "'--scores-labels -' had been given.  A '-' as a file name indicates standard\n" +
-        "input and can be used for any file argument that makes sense.\n\n" +
+        "input and can be used for any file argument that makes sense.\n" +
+        "\n" +
+
+        "File options can be appended to a file name to specify the columns of interest.\n" +
+        "The syntax is a colon followed by a dictionary having no spaces.  The keys of\n" +
+        "the dictionary are 'k' for key, 's' for score, and 'l' for label.  The values\n" +
+        "are column numbers or a list of column numbers for 'k' to use a compound key.\n" +
+        "See the examples below.\n" +
+        "\n" +
 
         "OPTIONS\n\n" +
 
@@ -101,21 +114,67 @@ public class Main {
         licenseKey + "\n" + indent +
         "Display a summary of the license for this software.\n" +
         debugKey + "\n" + indent + "Print stack traces, etc.\n" +
-        scoresKey + " FILE\n" + indent +
+        scoresKey + " FILE[:OPTS]\n" + indent +
         "File containing scores, one per line (CSV format).  Must be specified in\n" + indent +
-        "combination with --labels and are matched to the labels by line number.\n" + indent +
-        "No default.\n" +
-        labelsKey + " FILE\n" + indent +
+        "combination with --labels.  The scores are matched to the labels by the\n" + indent +
+        "specified key or otherwise by line number.  Accepts file options for key\n" + indent +
+        "and score columns.  No default.\n" +
+        labelsKey + " FILE[:OPTS]\n" + indent +
         "File containing labels, one per line (CSV format).  Labels specified by\n" + indent +
         "themselves are treated as already ranked from most positive to most\n" + indent +
-        "negative.  No default.\n" +
-        scoresLabelsKey + " FILE\n" + indent +
-        "File containing scores and labels, in two-column CSV format.  No\n" + indent +
-        "default.\n" +
+        "negative.  Accepts file options for key and label columns.  No default.\n" +
+        scoresLabelsKey + " FILE[:OPTS]\n" + indent +
+        "File containing scores and labels, in two-column CSV format.\n" + indent +
+        "Accepts file options for score and label columns.  No default.\n" +
+        "\n" +
+
+        "EXAMPLES\n\n" +
+
+        "Join results with ground truth:\n" +
+        "    ...Main --scores results.csv:{k:1,s:5} --labels truth.csv:{l:4,k:3}\n" +
+
+        "Join using compound key (whose fields are reversed in the truth):\n" +
+        "    ...Main --scores results.csv:{s:3,k:[1,2]} --labels truth.csv:{k:[2,1],l:3}\n" +
+
+        "Like above with shell quoting (bash):\n" +
+        "    ...Main --scores results.csv:'{k:1,s:5}' --labels 'truth.csv:{l:4,k:3}'\n" +
+
+        "Specify score and label columns:\n" +
+        "    ...Main --scores-labels scrsLbls.csv:{s:4,l:3}\n" +
+
+        "Options for standard input:\n" +
+        "    <classifier> | java...Main --scores-labels -:{s:102,l:103}\n" +
+
         "";  // This is here to make inserting/reordering lines easier
 
-    /** -h, -?, --help, help, etc. */
-    private static final Pattern helpPattern = Pattern.compile("(--?(h(elp)?|\\?))|help");
+    /**
+     * Pattern to recognize various requests for help: -h, -?, --help,
+     * help, etc.
+     */
+    private static final Pattern helpPattern =
+        Pattern.compile("(--?(h(elp)?|\\?))|help");
+
+    /**
+     * Pattern to recognize basic JSON-like dictionaries attached at the
+     * end of a string with a colon.
+     */
+    private static final Pattern okFileNameWithOptionsPattern =
+        Pattern.compile("(.+):\\{((?:[kls]:(?:\\d+|\\[\\d+(?:,\\d+)*\\]))(?:,[kls]:(?:\\d+|\\[\\d+(?:,\\d+)*\\]))*)\\}");
+
+    /**
+     * Pattern to extract individual key-value pairs from the basic
+     * JSON-like dictionaries.
+     */
+    private static final Pattern fileNameOptionPattern =
+        Pattern.compile(",?([kls]):(?:(\\d+)|\\[(\\d+(?:,\\d+)*)\\])");
+
+    /** Pattern to recognize a file with an attempted dictionary. */
+    private static final Pattern fileNameWithDictPattern =
+        Pattern.compile("(.+):(\\{.*?\\})");
+
+    /** Pattern to split by commas. */
+    private static final Pattern commaSplitPattern =
+        Pattern.compile("\\s*,\\s*");
 
     /**
      *
@@ -123,16 +182,16 @@ public class Main {
     public static void apiMain(String[] args, BufferedReader input,
                                PrintWriter output, PrintWriter error
                                ) throws MainException, FileNotFoundException, IOException {
-        // Environment.  For now this is a naive environment that maps
-        // keys (command line options) to unparsed values (or null).
-        // Booleans are represented by key existence and file names are
-        // strings.  It would be better to map the keys to appropriate
+        // Environment.  For now this is a somewhat naive environment
+        // that maps keys (command line options) to FileArguments or
+        // unparsed values (or null).  Booleans are represented by key
+        // existence.  It would be better to map the keys to appropriate
         // objects that can handle the various types of environment
-        // values.
+        // values, like in a command line parsing library.
         String defaultOperation = scoresLabelsKey;
         String defaultFileName = stdioFileName;
         String defaultDelimiter = ",";
-        Map<String, String> env = new TreeMap<String, String>();
+        Map<String, Object> env = new TreeMap<String, Object>();
 
         // Search the command line arguments for requested help.  Help
         // overrides all other operations and must be processed before
@@ -160,37 +219,43 @@ public class Main {
                        ) {
                 // Check next argument exists
                 if (argIndex + 1 < args.length) {
-                    String fileName = args[argIndex + 1];
-                    env.put(arg, fileName);
+                    FileArgument fileArg = parseFileNameWithOptions(args[argIndex + 1]);
+                    env.put(arg, fileArg);
                     argIndex++;
                 } else {
                     throw new MainException(String.format("Argument missing for option: %s", arg), ExitStatus.ERROR_USAGE);
                 }
             } else if (arg.equals(debugKey)) {
-                // Ignore
+                // Ignore.  Nothing to be done.
             } else {
                 throw new MainException(String.format("Unrecognized option: %s", arg), ExitStatus.ERROR_USAGE);
             }
         }
 
-        // Enforce logical constraints
+        // Enforce logical constraints between options
         if (env.containsKey(scoresKey) && !env.containsKey(labelsKey)) {
             throw new MainException(String.format("Option '%s' must be accompanied by option '%s'.", scoresKey, labelsKey), ExitStatus.ERROR_USAGE);
         }
 
-        // Check files exist up front before processing anything
-        for (String value : env.values()) {
-            if (value == null || value.equals(defaultFileName))
-                continue;
-            File file = new File(value);
-            if (!file.exists() || !file.isFile() || !file.canRead()) {
-                throw new MainException(String.format("Not a readable file: %s", value), ExitStatus.ERROR_FILE);
+        // Check files exist up front before processing anything.  (All
+        // files happen to be inputs.)
+        for (Object value : env.values()) {
+            if (value instanceof FileArgument) {
+                String fileName = ((FileArgument) value).name;
+                if (fileName.equals(defaultFileName))
+                    continue;
+                File file = new File(fileName);
+                if (!file.exists() || !file.isFile() || !file.canRead()) {
+                    throw new MainException(String.format("Not a readable file: %s", fileName), ExitStatus.ERROR_FILE);
+                }
             }
         }
 
         // If no operations, do the default
         if (env.size() == 0) {
-            env.put(defaultOperation, defaultFileName);
+            FileArgument defaultFile = new FileArgument();
+            defaultFile.name = defaultFileName;
+            env.put(defaultOperation, defaultFile);
         }
 
         // Process the operations
@@ -209,9 +274,9 @@ public class Main {
             // Input is scores and labels together
             //output.println(scoresLabelsKey);
             //output.println(env.get(scoresLabelsKey));
-            // TODO assume columns or what for multiple columns in single file?
             // Get scores and labels CSV input from a file or stdin as specified
-            BufferedReader scoresLabelsInput = openFileOrDefault(env.get(scoresLabelsKey), stdioFileName, input);
+            FileArgument slFile = (FileArgument) env.get(scoresLabelsKey);
+            BufferedReader scoresLabelsInput = openFileOrDefault(slFile.name, stdioFileName, input);
             // Generate curve and output
             processScoresLabelsOneInput(scoresLabelsInput, defaultDelimiter, 0, 1, output);
         } else if (env.containsKey(labelsKey)) {
@@ -223,10 +288,12 @@ public class Main {
                 //output.println(scoresKey);
                 //output.println(env.get(scoresKey));
                 // Get scores CSV input from a file or stdin as specified
-                scoresInput = openFileOrDefault(env.get(scoresKey), stdioFileName, input);
+                FileArgument sFile = (FileArgument) env.get(scoresKey);
+                scoresInput = openFileOrDefault(sFile.name, stdioFileName, input);
             }
             // Get labels CSV input from a file or stdin as specified
-            labelsInput = openFileOrDefault(env.get(labelsKey), stdioFileName, input);
+            FileArgument lFile = (FileArgument) env.get(labelsKey);
+            labelsInput = openFileOrDefault(lFile.name, stdioFileName, input);
             // Generate curve and output
             processScoresLabelsTwoInputs(scoresInput, labelsInput, defaultDelimiter, output);
         }
@@ -289,6 +356,57 @@ public class Main {
             }
             System.exit(exitStatus);
         }
+    }
+
+    public static FileArgument parseFileNameWithOptions(String token) throws MainException {
+        FileArgument fileArg = new FileArgument();
+        // Parse the file name and (possible) options
+        Matcher fileWOptsMatcher = okFileNameWithOptionsPattern.matcher(token);
+        Matcher fileWDictMatcher = fileNameWithDictPattern.matcher(token);
+        if (fileWOptsMatcher.matches()) {
+            fileArg.name = fileWOptsMatcher.group(1);
+            String options = fileWOptsMatcher.group(2);
+            Matcher optMatcher = fileNameOptionPattern.matcher(options);
+            while (optMatcher.find()) {
+                // TODO worry about keys specified multiple times?
+                String key = optMatcher.group(1);
+                switch (key.charAt(0)) {
+                case 'k':
+                    String[] colStrings = commaSplitPattern.split(optMatcher.group(3));
+                    int[] cols = new int[colStrings.length];
+                    for (int colIndex = 0; colIndex < cols.length; colIndex++) {
+                        cols[colIndex] = Integer.parseInt(colStrings[colIndex]);
+                    }
+                    fileArg.keyCols = cols;
+                    break;
+                case 'l':
+                    fileArg.labelCol = Integer.parseInt(optMatcher.group(2));
+                    break;
+                case 's':
+                    fileArg.scoreCol = Integer.parseInt(optMatcher.group(2));
+                    break;
+                default:
+                    String msg = String.format("File option key not in {k,l,s}: '%s' in '%s'", key, options);
+                    throw new MainException(msg, ExitStatus.ERROR_USAGE);
+                }
+            }
+        } else if (fileWDictMatcher.matches()) {
+            String msg = String.format("Bad options for file '%s': %s",
+                                       fileWDictMatcher.group(1),
+                                       fileWDictMatcher.group(2));
+            throw new MainException(msg, ExitStatus.ERROR_USAGE);
+        } else {
+            // Apparently no options so the whole token is the file name
+            fileArg.name = token;
+        }
+        return fileArg;
+    }
+
+    private static class FileArgument {
+        public String name;
+        public int[] keyCols;
+        public int scoreCol;
+        public int labelCol;
     }
 
     public static BufferedReader openFileOrDefault(String fileName, String defaultName, BufferedReader defaultInput) throws IOException {
