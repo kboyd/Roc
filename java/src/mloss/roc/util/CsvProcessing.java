35492d89cbe527256d1ec6ee0545f68a239d2bad
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2014-04-24 20:10:05 -0500

    Documented CsvProcessing.
    * Removed old import.

--- java/src/mloss/roc/util/CsvProcessing.java
+++ java/src/mloss/roc/util/CsvProcessing.java
@@ -5,7 +5,6 @@
 
 package mloss.roc.util;
 
-import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -20,10 +19,31 @@ import java.util.List;
  */
 public class CsvProcessing {
 
+    /**
+     * Comparator for CSV rows (and similar) that uses certain fields as
+     * (compound) keys for comparison.  Can be used to sort a single set
+     * of rows like in sorting or compare two sets of rows like in
+     * joining.
+     */
     public static class StringArrayComparator implements Comparator<String[]> {
         int[] keyFields1;
         int[] keyFields2;
 
+        /**
+         * Creates a comparator for CSV rows that compares rows
+         * according to the given (possibly compound) keys.  The fields
+         * are matched in the given order.  For example, {4, 1, 2} and
+         * {3, 0, 1} specify the equality constraint: csv1.4 == csv2.3
+         * and csv1.1 == csv2.0 and csv1.2 == csv2.1.  To create a
+         * comparator to order a single set of rows make {@code
+         * keyFields1 == keyFields2} as the two CSV rows will be from
+         * the same source.
+         *
+         * @param keyFields1 The indices of the fields to use as keys
+         * from the first CSV input
+         * @param keyFields2 The indices of the fields to use as keys
+         * from the second CSV input
+         */
         public StringArrayComparator(int[] keyFields1, int[] keyFields2) {
             // Check the arrays are of the same length
             if (keyFields1.length != keyFields2.length) {
@@ -50,6 +70,15 @@ public class CsvProcessing {
         }
     }
 
+    /**
+     * Projects the fields from the given inputs into a single tuple.
+     *
+     * @param fields The selected fields.  The outer array is a list of
+     * fields to include.  Each inner array is a pair {@code
+     * {inputIndex, fieldIndex}} accessing the field {@code
+     * tuples[inputIndex][fieldIndex]}.
+     * @param tuples The inputs to project.
+     */
     public static String[] project(int[][] fields, String[]... tuples) {
         int length = fields.length;
         String[] result = new String[length];
@@ -61,6 +90,32 @@ public class CsvProcessing {
         return result;
     }
 
+    // TODO projectAll?
+
+    /**
+     * Performs an inner join of the given CSV inputs using the merge
+     * join algorithm.  Sorts the given inputs by their (compound) keys
+     * and then merge joins them, yielding the desired projection of
+     * fields.
+     *
+     * @param keys The keys (and, hence, equality constraints) for the
+     * join.  The outer array is a list of equality constraints.  Each
+     * inner array specifies an equality constraint by listing the keys
+     * that should be equal.  Each element of an inner array is a field
+     * index for the corresponding CSV input.  Their order and number
+     * matches that of the inputs.  For example, {@code {{3, 2}, {0,
+     * 1}}} sets up the following compound equality constraint for the
+     * join: csv1.3 == csv2.2 and csv1.0 == csv2.1.
+     * @param fields The fields to include in each result tuple, the
+     * desired projection.  The outer array lists the fields.  Each
+     * inner array is a pair {@code {inputIndex, fieldIndex}} that
+     * specifies a field to include in the output.  The fields are
+     * output in the given order.  For example, {{1, 3}, {1, 2}, {0, 0},
+     * {0, 5}} yields tuples consisting of the fields (csv2.3, csv2.2,
+     * csv1.0, csv1.5).
+     * @param csv1 The first CSV input
+     * @param csv2 The second CSV input
+     */
     public static List<String[]> mergeJoin(
             int[][] keys,
             int[][] fields,
