1d2dda4e05fe683d9d9539b73896e983684ee6d3
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2014-06-30 16:07:32 -0500

    Added precision-recall points report.
    * Added 'prPts' report that prints the points of the
      precision-recall curve.
    * Added PR area and points to the YAML report.

--- java/src/mloss/roc/Reports.java
+++ java/src/mloss/roc/Reports.java
@@ -14,12 +14,21 @@ import java.io.PrintWriter;
 public class Reports {
 
     public static void report(String reportName, Curve curve, PrintWriter output) {
-        if (reportName.equals("rocpts")) {
+        if (reportName.equals("prpts")) {
+            prPts(curve, output);
+        } else if (reportName.equals("rocpts")) {
             rocPts(curve, output);
         } else if (reportName.equals("yaml")) {
             yaml(curve, output);
         } else {
             throw new IllegalArgumentException(String.format("Report name not recognized: %s", reportName));
+        }
+    }
+
+    public static void prPts(Curve curve, PrintWriter output) {
+        double[][] prPoints = curve.prPoints();
+        for (double[] point : prPoints) {
+            output.println(String.format("%s %s", point[0], point[1]));
         }
     }
 
@@ -42,6 +51,13 @@ public class Reports {
             // because I can't find a floating point format that works
             // like the string formatting and chops off the trailing
             // zeros.
+            output.println(String.format("  - [%s, %s]", point[0], point[1]));
+        }
+        output.println("PR area: " + curve.prArea());
+        double[][] prPoints = curve.prPoints();
+        output.println(String.format("PR points count: %d", prPoints.length));
+        output.println("PR points:");
+        for (double[] point : prPoints) {
             output.println(String.format("  - [%s, %s]", point[0], point[1]));
         }
         output.println("ROC points Gnuplot text: |");
